114ab0defcd634ce4dab11678f0bd067846ec3a1
Tomualdo <tomas.krajcoviech@gmail.com>
2020-05-05 20:28:55 +0200

    added some conditions

--- script.sh
+++ script.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-wget https://github.com/webchain-network/miner/releases/download/v2.8.0/webchain-miner-2.8.0-linux-amd64.tar.gz
+wget -nv https://github.com/webchain-network/miner/releases/download/v2.8.0/webchain-miner-2.8.0-linux-amd64.tar.gz 2>/dev/null && echo "files downloaded" || echo "failed to download !!"
+[ -f webchain-miner-2.8.0-linux-amd64.tar.gz ] || exit 1
+
 tar xfz webchain-miner-2.8.0-linux-amd64.tar.gz
 
-sed -i 's/YOUR_WALLET/0x7fc9c901af13b5f1b071139843c1c4185063b2c8/g' config.json
-echo "wallet updated"
+update(){
+	sed -i 's/YOUR_WALLET/0x7fc9c901af13b5f1b071139843c1c4185063b2c8/g' config.json
+	sed -i 's/pool.webchain.network:3333/pool.webchain.network:2222/g' config.json
+	sed -i 's/donate-level": 5/donate-level": 1/g' config.json
+	sed -i 's/"max-cpu-usage": 75,/"max-cpu-usage": 95,/g' config.json
+	}
 
-sed -i 's/pool.webchain.network:3333/pool.webchain.network:2222/g' config.json
-echo "pool updated"
-
-sed -i 's/donate-level": 5/donate-level": 1/g' config.json
-sed -i 's/"max-cpu-usage": 75,/"max-cpu-usage": 95,/g' config.json
+update && echo "Updated SUCCESFULLY !\nNow run \e[1m./webchain-miner\e[0m" || echo "Update failed !!! ..."
